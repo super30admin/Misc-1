f4705d3c6e5aacf278fdbfa91f459123f4b44ba6
Manikandan Selvanathan <mskagesai.com>
2021-08-03 20:23:53 -0400

    Problem 2 completed

--- Coindistribution.java
+++ Coindistribution.java
@@ -1,3 +1,29 @@
 public class Coindistribution {
     
+    int steps;
+    public int distributeCoins(TreeNode root) {
+        if(root == null) return 0;
+        steps = 0;
+        helper(root);
+        return steps;
+    }
+    
+    //
+    //TC: O(N) Visiting all the nodes
+    //SC: O(H) in the recursive stack maximum it would be height of the tree.
+    private int helper(TreeNode root) {
+        if(root == null) { return 0; }
+        
+        int remainingCoins = root.val -1;
+        remainingCoins += helper(root.left);
+        remainingCoins += helper(root.right);
+        steps += Math.abs(remainingCoins);
+        return remainingCoins;
+    }
+    
+    public static void main(String[] args) {
+        Coindistribution coindistribution = new Coindistribution();
+        int result = coindistribution.distributeCoins(new TreeNode().getSampleBST())
+        System.out.println("The result: "+result);
+    }
 }
