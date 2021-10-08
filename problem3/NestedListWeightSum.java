5c71107370ae5e115c4efc0c96cb56fa2ec7e238
Srirag <78050630+sriragcs@users.noreply.github.com>
2021-10-07 20:34:23 -0700

    Completed Misc-1

--- problem3/NestedListWeightSum.java
+++ problem3/NestedListWeightSum.java
@@ -4,9 +4,12 @@
 // Any problem you faced while coding this : No
 package problem3;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class NestedListWeightSum {
+	/********************* DFS *********************/
 	public int depthSum(List<NestedInteger> nestedList) {
 		return helper(nestedList, 1);
 	}
@@ -18,6 +21,39 @@ public class NestedListWeightSum {
 				sum += num.getInteger() * depth;
 			} else {
 				sum += helper(num.getList(), depth + 1);
+			}
+		}
+		return sum;
+	}
+
+	/********************* DFS *********************/
+	public int depthSumBFS(List<NestedInteger> nestedList) {
+		if (nestedList == null || nestedList.size() == 0) {
+			return 0;
+		}
+
+		int sum = 0;
+
+		Queue<Pair> queue = new LinkedList<>();
+
+		for (NestedInteger num : nestedList) {
+			queue.add(new Pair(num, 1));
+		}
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Pair p = queue.poll();
+				NestedInteger num = p.num;
+				int level = p.level;
+
+				if (num.isInteger()) {
+					sum += num.getInteger() * level;
+				} else {
+					for (NestedInteger curr : num.getList()) {
+						queue.add(new Pair(curr, level + 1));
+					}
+				}
 			}
 		}
 		return sum;
