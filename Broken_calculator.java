61f343d6186ba4251c7286bb997eb2510cbc4841
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-01-10 17:03:35 -0500

    Create Broken_calculator.java

---
+++ Broken_calculator.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int count = 0;
+        
+        while(target > startValue){
+            if(target % 2 == 0){
+                target = target/2;
+                
+            }else{
+                target += 1;
+            }
+            count++;
+        }
+        return count + startValue - target;
+    }
+}
+
+//TC :O(log(n))
+//SC: O(1)
